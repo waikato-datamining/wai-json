da0a430e836d037b26e89fc02ddeef06f1651fb3
csterling <coreytsterling@gmail.com>
2020-02-21 12:15:09 +1300

    Added the ability to pretty-serialise.

--- src/wai/json/serialise/_JSONSerialisable.py
+++ src/wai/json/serialise/_JSONSerialisable.py
@@ -1,6 +1,6 @@
 import json
 from abc import abstractmethod
-from typing import IO
+from typing import IO, Optional
 
 from wai.common.decorator import ensure_error_type
 
@@ -40,32 +40,51 @@ class JSONSerialisable:
         return raw_json
 
     @ensure_error_type(JSONSerialisationError, "Error converting {self.__class__.__qualname__} to JSON string: {0}")
-    def to_json_string(self, validate: bool = True) -> str:
+    def to_json_string(self, indent: Optional[int] = None, validate: bool = True) -> str:
         """
         Serialises this object to a JSON-format string.
 
+        :param indent:      The indent level to use for pretty-printing, or
+                            None for compact representation.
         :param validate:    Whether to validate the serialised JSON if possible.
         :return:            The JSON string.
         """
-        return json.dumps(self.to_raw_json(validate))
+        if indent is not None and indent < 0:
+            raise JSONSerialisationError(f"Indent level for pretty-printing must be non-negative, "
+                                         f"got {indent}")
+
+        return json.dumps(self.to_raw_json(validate),
+                          indent=indent,
+                          separators=(',', ':') if indent is None else None)
 
     @ensure_error_type(JSONSerialisationError, "Error writing {self.__class__.__qualname__} to stream: {0}")
-    def write_json_to_stream(self, stream: IO[str], validate: bool = True) -> None:
+    def write_json_to_stream(self, stream: IO[str], indent: Optional[int] = None, validate: bool = True) -> None:
         """
         Writes this object as JSON to a string-stream.
 
-        :param validate:    Whether to validate the serialised JSON if possible.
         :param stream:      The stream to write to.
+        :param indent:      The indent level to use for pretty-printing, or
+                            None for compact representation.
+        :param validate:    Whether to validate the serialised JSON if possible.
         """
-        json.dump(self.to_raw_json(validate), stream)
+        if indent is not None and indent < 0:
+            raise JSONSerialisationError(f"Indent level for pretty-printing must be non-negative, "
+                                         f"got {indent}")
+
+        json.dump(self.to_raw_json(validate),
+                  stream,
+                  indent=indent,
+                  separators=(',', ':') if indent is None else None)
 
     @ensure_error_type(JSONSerialisationError, "Error saving {self.__class__.__qualname__} to '{filename}': {0}")
-    def save_json_to_file(self, filename: str, validate: bool = True) -> None:
+    def save_json_to_file(self, filename: str, indent: Optional[int] = None, validate: bool = True) -> None:
         """
         Saves this object to the given file.
 
-        :param validate:    Whether to validate the serialised JSON if possible.
         :param filename:    The name of the file to save to.
+        :param indent:      The indent level to use for pretty-printing, or
+                            None for compact representation.
+        :param validate:    Whether to validate the serialised JSON if possible.
         """
         with open(filename, 'w') as file:
-            self.write_json_to_stream(file, validate)
+            self.write_json_to_stream(file, indent, validate)
